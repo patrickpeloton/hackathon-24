7fc3f77b6ca73ebc205666df30d74948e604e86f
patrickpeloton <patrick.trasborg@onepeloton.com>
2024-09-09 14:21:23 -0400

    try to read metrics with next app

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,75 @@
-import Image from "next/image";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+
+let CfuTreadSensor: any;
 
 export default function Home() {
+  const [speed, setSpeed] = useState(0);
+  const [incline, setIncline] = useState(0);
+  const [targetIncline, setTargetIncline] = useState(0);
+
+  function withTreadMetrics() {
+    let cfu: any = null;
+    try {
+      // request updates at 30Hz (30 times/second).
+      cfu = new CfuTreadSensor({ frequency: 30 });
+      // current implementation will not report any errors from the cfu
+      // but chromium can report errors for permissions and such
+      cfu.addEventListener("error", (event: any) => {
+        if (event.error.name == "NotAllowedError") {
+          // seems like permissions have been revoked by the user
+          console.log("Permissions revoked.");
+        } else {
+          // some other error occured, just log it
+          console.log("Error reading sensor: " + event.error.name);
+        }
+      });
+
+      // add event listener to start receiving sensor input
+      cfu.addEventListener("reading", () => {
+        console.log(
+          `Bike data received speed ${cfu.speed}, incline ${cfu.incline}, target incline ${cfu.targetIncline}`
+        );
+        // let's update the DOM with new values
+        setSpeed(cfu?.speed);
+        setIncline(cfu?.incline);
+        setTargetIncline(cfu?.targetIncline);
+      });
+
+      if (cfu != null) {
+        cfu.start();
+      }
+    } catch (err) {
+      console.log("Error opening tread " + err);
+    }
+  }
+
+  useEffect(() => {
+    // not sure if we need to run this permissions code
+    // if (navigator.permissions != null) {
+    //   // we need to ask the user for permissions to access the sensor
+    //   navigator.permissions
+    //     .query({ name: "cfu-tread" })
+    //     .then((result) => {
+    //       if (result.state == "denied") {
+    //         console.log("Permission to use CFU sensor is denied.");
+    //         return;
+    //       }
+
+    //       withTreadMetrics();
+    //     });
+    // } else {
+    withTreadMetrics();
+    // }
+  }, []);
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div>Speed: {speed}</div>
+      <div>Incline: {incline}</div>
+      <div>Target Incline: {targetIncline}</div>
     </div>
   );
 }
